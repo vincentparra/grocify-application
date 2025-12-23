3def2ca51e0fa56524f01c9f6595b05af5a4685b
vincentparra <vincent.jovan.resurreccion.parra@gmail.com>
2025-12-23 10:13:31 +0800

    GA-12-Create-Recipe-API (#3)

--- src/app/controller/recipe/RecipeController.js
+++ src/app/controller/recipe/RecipeController.js
@@ -1,27 +1,66 @@
 import DB from "../../config/db.js";
 import User from "../../model/User/UserModel.js";
 import Recipes from "../../model/Recipe/RecipeModel.js";
-import findRecipebyUserName from "../../repository/recipe/RecipeRepository.js";
+import RecipeRepository from "../../repository/recipe/RecipeRepository.js";
+import UserRepository from "../../repository/user/UserRepository.js";
 
 async function findRecipe(req, res) {
-  DB.Connection();
-  const { username } = req.body;
-  const userRecipe = await findRecipebyUserName(username, res);
-  if (userRecipe.length === 0) {
-    return res.status(404).json({ message: "No recipe found for the user" });
-  }
-  const recipe = userRecipe.map((r) => ({
-    name:
-      r.user.person.first_name +
-      " " +
-      r.user.person.middle_name +
-      " " +
-      r.user.person.last_name,
-    instruction: r.instruction,
-    ingredients: r.ingredients,
-  }));
+  try {
+    DB.Connection();
+    const { username } = req.principal.UserPrincipal;
+    const userRecipe = await RecipeRepository.findRecipebyUserName(
+      username,
+      res
+    );
+    if (userRecipe.length === 0) {
+      return res.status(404).json({ message: "No recipe found for the user" });
+    }
+    const recipe = userRecipe.map((r) => ({
+      name:
+        r.user.person.first_name +
+        " " +
+        r.user.person.middle_name +
+        " " +
+        r.user.person.last_name,
+      instruction: r.instruction,
+      ingredients: r.ingredients,
+      title: r.title,
+    }));
 
-  res.status(200).json(recipe);
+    res.status(200).json(recipe);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+  }
 }
 
-export default findRecipe;
+async function createRecipe(req, res) {
+  try {
+    DB.Connection();
+    const { description, ingredients, title } = req.body;
+    const { username } = req.principal.UserPrincipal;
+    const user = await UserRepository.findUserByUsername(username);
+    if (!user) {
+      return res.status(404).json({
+        message: "User no found",
+      });
+    }
+
+    const createdRecipe = await RecipeRepository.createRecipe(
+      user,
+      description,
+      ingredients,
+      title
+    );
+    if (!createRecipe) {
+      return res.status(400).json({
+        message: "BAD REQUEST",
+      });
+    }
+    res.status(201).json(createdRecipe);
+  } catch (error) {
+    res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+  }
+}
+
+export default { findRecipe, createRecipe };
