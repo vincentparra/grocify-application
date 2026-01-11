eb718cff86894ebfafd6a14945abcb6d352be08e
vincentparra <vincent.jovan.resurreccion.parra@gmail.com>
2026-01-11 21:47:09 +0800

    GA-15-Implement-login-attempts-service (#6)
    * added login attempts limits
    
    * added login attempts limits

--- src/app/routes/AuthRoute.js
+++ src/app/routes/AuthRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
 import LoginController from "../controller/authentication/LoginController.js";
 import UserRegistration from "../controller/authentication/UserRegistration.js";
+import loginAttemptLimiter from "../utils/security/limiter/LoginAttempts.js";
 const router = express.Router();
-
-router.post("/login", LoginController.login);
+router.post("/login", loginAttemptLimiter, LoginController.login);
 router.post("/register", UserRegistration);
 export default router;
